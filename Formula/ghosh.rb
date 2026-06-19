7bd616411cc238b83f62babaccd44e2241204a2a
Mike Starr <go2starr@gmail.com>
2026-06-18 17:06:16 -0700

    Switch ghosh to binary formula

--- Formula/ghosh.rb
+++ Formula/ghosh.rb
@@ -1,30 +1,16 @@
 class Ghosh < Formula
   desc "Mosh-style remote shell over QUIC"
   homepage "https://github.com/go2starr/dream/tree/codex/quic-broker-jsonrpc/projects/ghosh"
-  url "https://raw.githubusercontent.com/go2starr/homebrew-tap/main/resources/ghosh-0.1.0-d6b65b8.tar.gz"
-  sha256 "932e50ce37b73d57ae20e090811e518626679ebd8b277faa1cd7c8aef2102730"
+  url "https://raw.githubusercontent.com/go2starr/homebrew-tap/main/resources/ghosh-0.1.0-macos-arm64.tar.gz"
+  sha256 "2f29180f899e3604d2f7256286a6bf94ae8e10f0231f0fd1c75a4efc87ffaa92"
   version "0.1.0"
 
-  depends_on "llvm" => :build
-  depends_on "rust" => :build
-
-  resource "ghostty-vt" do
-    url "https://raw.githubusercontent.com/go2starr/homebrew-tap/main/resources/ghostty-vt-macos-arm64-f9c52d0cd.tar.gz"
-    sha256 "98c03bb2894e87220edbad0fc293cdd51447c2c28bd9687a835e959043e9d5e1"
-  end
-
   def install
-    odie "this prebuilt libghostty-vt package is currently macOS arm64 only" unless OS.mac? && Hardware::CPU.arm?
+    odie "this prebuilt ghosh package is currently macOS arm64 only" unless OS.mac? && Hardware::CPU.arm?
 
-    vt_dir = buildpath/"ghostty-vt"
-    vt_dir.mkpath
-    resource("ghostty-vt").stage vt_dir
-
-    ENV["GHOSTTY_VT_DIR"] = vt_dir
-    ENV["LIBCLANG_PATH"] = Formula["llvm"].opt_lib/shared_library("libclang")
-    ENV.prepend_path "PATH", Formula["llvm"].opt_bin
-
-    system "cargo", "install", *std_cargo_args(path: "ghosh")
+    bin.install "bin/ghosh"
+    bin.install "bin/ghosh-server"
+    doc.install Dir["share/doc/ghosh/*"]
   end
 
   test do
